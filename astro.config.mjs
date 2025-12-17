a2faebee2cc62f171558f27b64cf4499585929b6
Shailesh Rawat <you@example.com>
2025-12-17 14:14:59 +0530

    chore: publish prose

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+const isDev = process.env.NODE_ENV === 'development';
+const site = isDev ? 'http://localhost:4321' : 'https://pruning-my-pothos.github.io/prose/';
+const base = isDev ? '/' : '/prose';
+
 export default defineConfig({
-  site: 'https://pruning-my-pothos.github.io/prose/',
-  base: '/prose',
+  site,
+  base,
   integrations: [sitemap()],
 });
